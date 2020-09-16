7c6d8829eb809ea1d1195ab815e41bf0bccabe38
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-09-16 18:27:39 +0200

    refactor: learning how to de-reference the hash-structure to extract data
    ... such as the question and the string of the correct answer

--- src/main.pl
+++ src/main.pl
@@ -2,19 +2,35 @@
 use v5.32;
 use warnings;
 use diagnostics;
-use Andiluca::Useful ("title", "assert");   # Module with various useful code snippets
-use Regexp::Grammars;                       # Module for using Grammars in Regex
-use Data::Show;                             # Module for showing content of variables such as hashes
+use lib ('lib');                          # Includes local lib-folder -> for custom modules 'Andiluca::...'
+use Andiluca::Useful ("title", "assert"); # Module with various useful code snippets
+use Regexp::Grammars;                     # Module for using Grammars in Regex
+use Data::Show;                           # Module for showing content of variables such as hashes
 # use File::Path;    # TODO:  maybe implement this for consistent behaviour on windows?
 
 
-my $file = "short_exam_master_file.txt";
-my $path = "AssignmentDataFiles/MasterFiles/" . $file;
+my $path;
+
+# Open given file (or standard file):
+if (@ARGV == 0) {
+    # die "Please provide a filepath, such as 'perl src/main.pl AssignmentDataFiles/MasterFiles/short_exam_master_file.txt'\n";
+
+    # Use standard-file:
+    my $file = "FHNW_entrance_exam_master_file_2017.txt";
+    $path = "AssignmentDataFiles/MasterFiles/" . $file;
+}
+else {
+    # Take provided filepath:
+    $path = $ARGV[0];
+}
 
 
 # Open and read the whole file:
 open my $fh, '<', $path or die "Cannot read '$path': $!\n";
-my $bare_content = do { local $/; readline($fh) };
+my $bare_content = do {
+    local $/;
+    readline($fh)
+};
 close $fh;
 
 
@@ -63,20 +79,79 @@ my $exam_parser = qr{
         \s* \n
 }xms;
 
-
-
 my %parsed;
 
 # Run the regex-grammar to create the hash-object:
 if ($bare_content =~ $exam_parser) {
-    %parsed = %/;      # '%/' is a special variable from the Regexp-module to fetch the created data-structure
-    show (%parsed);
+    %parsed = %/; # '%/' is a special variable from the Regexp-module to fetch the created data-structure
+
+    # Show the whole data structure:
+    # show (%parsed);
+
+    # Variant 1: Direct option to read the first decoration line:
+    # print $parsed{'exam'}->{'exam_component'}->[0]->{'decoration'};  # reads the first decoration line!
+
+    # Variant 2: to read the first decoration line:
+    my @arr = @{$parsed{'exam'}->{'exam_component'}}; # Array containing references to the hashes!
+    # print $arr[0]->{'decoration'};
+
+    # Variant 3: - with more de-referencing:
+    # my %hash1 = %{$arr[0]};
+    # print $hash1{'decoration'};
+
+
+    # ----------------------------------- #
+    # Save all questions in a hash-array: #
+    # ----------------------------------- #
+
+    #  DATA-STRUCTURE for 'question_and_answers':
+    #      {
+    #       exam => {
+    #         exam_component => [
+    #           {
+    #             question_and_answers => {
+    #               answer   => [
+    #                             { checkbox => "[X]", text => " List, scalar, and void\n" },
+    #                             { checkbox => "[ ]", text => " List, linear, and void\n" },
+    #                             { checkbox => "[ ]", text => " List, scalar, and null\n" },
+    #                             { checkbox => "[ ]", text => " Null, scalar, and void\n" },
+    #                             { checkbox => "[ ]", text => " Blood, sweat, and tears\n" },
+    #                           ],
+    #               question => {
+    #                             question_number => "2.",
+    #                             text => " Perl's three main types of call context (or \"amount context\") are:\n",
+    #                           },
+    #             },
+    #           },
+    #
+    #
+    my @all_questions;
+    for my $ref (@arr) {
+        my %entry = %{$ref};
+        if (exists($entry{"question_and_answers"})) {
+            push(@all_questions, $entry{"question_and_answers"});
+        }
+    }
+
+    # Variant 1: Getting one question:
+    # print $all_questions[0]->{"question"}->{"text"};
+
+    # Variant 2:
+    my %question0 = %{$all_questions[0]};
+    print $question0{"question"}->{"text"};
+
+    # Getting the first CORRECT answer:
+    my $correct_answer0;
+    for my $answer (@{$question0{'answer'}}) {
+        if ($answer->{'checkbox'} eq "[X]") {
+            $correct_answer0 = $answer->{text};
+        }
+    }
+    print " -> CORRECT ANSWER:  $correct_answer0";
+
 }
 else {
     warn 'Not a valid exam file';
 }
 
 
-# Testing title and assert-functionality:
-title("Using title module:");
-assert("true", "true", "simple test");
