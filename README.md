fa350c373da2c1d712c6589819891c479bd23f1a
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2020-09-25 17:38:14 +0200

    feat: generation of exams working for multiple files now
    .. including file-globbing

--- README.md
+++ README.md
@@ -13,9 +13,12 @@ Luca Fluri - https://github.com/lucafluri
 
 ## Usage
 - For creating empty tests (with randomized order of answers of each question), use the script `main.pl`:
-  - If no arguments are provided, the script runs with a default file: `perl src/main.pl`
+  - If the user doesn't provide an argument, all the *.txt in the folder 'AssignmentDataFiles/MasterFiles/'
+    are processed for this script: `perl src/main.pl`
+  - The user can provide one or multiple files as parameter 
   - If you provide one argument, the script runs with the given file. Example: `perl src/main.pl AssignmentDataFiles/MasterFiles/short_exam_master_file.txt`
   - The generated empty tests will be created in a subfolder named `/Generated`, inside the directory of the provided file.
+    Note: This subdirectory will be created if not already present.
 - For generating the score of all the files, use the script `score.pl`:
   - First parameter: Master-file
   - Second parameter: Student-file(s)
@@ -23,7 +26,11 @@ Luca Fluri - https://github.com/lucafluri
 
 
 ### Caveats
-There is one file which behaves very different on the computer of Luca (with Ubuntu) and my computer (macOS):
-If we both run the command 
-`perl src/score.pl AssignmentDataFiles/MasterFiles/FHNW_entrance_exam_master_file_2017.txt AssignmentDataFiles/SampleResponses/20170828-092520-FHNW_entrance_exam-ID039411` (which compares the exam-file with id "ID039411" with the master-file), we get a different result, which we could not 
-find an explanation for.
+
+- There is one file which behaves very different on the computer of Luca (with Ubuntu) and my computer (macOS):
+  - If we both run the command 
+    `perl src/score.pl AssignmentDataFiles/MasterFiles/FHNW_entrance_exam_master_file_2017.txt AssignmentDataFiles/SampleResponses/20170828-092520-FHNW_entrance_exam-ID039411` (which compares the exam-file with id "ID039411" with the master-file), we get a different result, which we could not 
+    find an explanation for.
+  - The other 35 files in AssignmentDataFiles/SampleResponses seem to behave identically on both computers.
+
+
--- src/main.pl
+++ src/main.pl
@@ -2,6 +2,7 @@
 use v5.32;
 use warnings;
 use diagnostics;
+use experimental ('signatures');
 use Data::Show;                           # Module for showing content of variables such as hashes
 use Term::ANSIColor ('color', 'colored');
 
@@ -14,63 +15,71 @@ use Andiluca::Various("parse_header", "parse_decoration_divider", "get_current_d
 use Andiluca::Exam_Parser("parsing_exam");
 
 
-my $path;
+my @files;
 
-# Open given file (or standard file, if no argument is passed):
+# If the user doesn't provide an argument, all the *.txt in the folder 'AssignmentDataFiles/MasterFiles/' are used:
 if (@ARGV == 0) {
-    # die "Please provide a filepath, such as 'perl src/main.pl AssignmentDataFiles/MasterFiles/short_exam_master_file.txt'\n";
-
-    # Use one of the standard-files:
-    my $filename = "FHNW_entrance_exam_master_file_2017.txt";
-    # my $file = "short_exam_master_file.txt";
-
-    # Construct the path:
-    $path = "AssignmentDataFiles/MasterFiles/" . $filename;
+    @files = glob("AssignmentDataFiles/MasterFiles/*.txt");
 }
+# Otherwise, a user can provide one or multiple file-paths to specific master-files:
 else {
-    # Take first provided argument as filepath:
-    $path = $ARGV[0];
+    for my $file (@ARGV){
+        push (@files, $file);
+    }
 }
 
-say colored([ 'yellow' ], "This script generates an exam based on the file '$path'.");
-
-
-# Open and read the whole file:
-my $bare_content = read_file($path);
-
-
-# Parse the various needed elements:
-my $parsed_ref = parsing_exam($bare_content);
-my %parsed = %{$parsed_ref};
-my $header = parse_header($bare_content);
-my $decoration_line = parse_decoration_divider($bare_content);
-
-
-# Show the whole data structure:
-# show (%parsed);
-
-# For understanding the data-structure: just print out all the questions and their respective correct answer:
-# understand_data_structure1(\%parsed);
-
-
-# Create empty random exam file:
-my $randomized = create_empty_random_exam(\%parsed, $header, $decoration_line);
-my $date_and_time = get_current_date_time_string();
-
-# Create the path for the output-file:
-# The path is generally the same, but with an added subfolder /Generated
-# And the file name gets prefixed with date and time.
-# Sample input:
-#     AssignmentDataFiles/MasterFiles/FHNW_entrance_exam_master_file_2017.txt
-# Sample output would be:
-#     AssignmentDataFiles/MasterFiles/Generated/20170904-132602-FHNW_entrance_exam_master_file_2017.txt
-$path =~ s%/([^/]*)$%/Generated/$date_and_time-$1%;
-
-# Write the file to the disk
-open(my $out_fh, ">", $path) // die colored([ 'red' ], "\nUnable to open file for write-access '$path':\n\t$!\n");
-say {$out_fh} $randomized;
-say colored([ 'green' ], "Empty Exam-File created in '$path'.");
-close($out_fh);
+for my $file (@files) {
+    generate_exam($file);
+}
 
 
 
+sub generate_exam($file_path) {
+    say colored([ 'yellow' ], "Generating exam based on the file '$file_path':");
+
+    # Open and read the whole file:
+    my $bare_content = read_file($file_path);
+
+
+    # Parse the various needed elements:
+    my $parsed_ref = parsing_exam($bare_content);
+    my %parsed = %{$parsed_ref};
+    my $header = parse_header($bare_content);
+    my $decoration_line = parse_decoration_divider($bare_content);
+
+
+    # Show the whole data structure:
+    # show (%parsed);
+
+
+    # For understanding the data-structure: just print out all the questions and their respective correct answer:
+    # understand_data_structure1(\%parsed);
+
+
+    # Create empty random exam file:
+    my $randomized = create_empty_random_exam(\%parsed, $header, $decoration_line);
+    my $date_and_time = get_current_date_time_string();
+
+    # Create subdirectory /Generated, if not yet present:
+    $file_path =~ m%(.*(.*/)*/)[^/]*$%;
+    my $path_generated = $1 . "/Generated";
+    if (!-d $path_generated) {
+        mkdir ($path_generated);
+    }
+
+    # Create the path for the output-file:
+    # The path is generally the same, but with an added subfolder /Generated
+    # And the file name gets prefixed with date and time.
+    # Sample input:
+    #     AssignmentDataFiles/MasterFiles/FHNW_entrance_exam_master_file_2017.txt
+    # Sample output would be:
+    #     AssignmentDataFiles/MasterFiles/Generated/20170904-132602-FHNW_entrance_exam_master_file_2017.txt
+    $file_path =~ s%/([^/]*)$%/Generated/$date_and_time-$1%;
+
+
+    # Write the file to the disk
+    open(my $out_fh, ">", $file_path) // die colored([ 'red' ], "\nUnable to open file for write-access '$file_path':\n\t$!\n");
+    say {$out_fh} $randomized;
+    say colored([ 'green' ], "       Empty Exam-File created in '$file_path'.");
+    close($out_fh);
+}
